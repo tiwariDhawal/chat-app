307ce075976535aacc88e2cf8e330a6204a0765a
tiwariDhawal <98008402+tiwariDhawal@users.noreply.github.com>
2022-06-26 09:17:04 +0530

    initialised firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+const config = {
+  apiKey: "AIzaSyCQPgZog03ChcIfP5tPm5myR4c6lcQhes0",
+  authDomain: "chat-web-app-3213a.firebaseapp.com",
+  databaseURL: "https://chat-web-app-3213a-default-rtdb.firebaseio.com",
+  projectId: "chat-web-app-3213a",
+  storageBucket: "chat-web-app-3213a.appspot.com",
+  messagingSenderId: "972852695183",
+  appId: "1:972852695183:web:9aa42d78bc58ab68e460fa",
+};
+
+const app = firebase.initializeApp(config);
+
