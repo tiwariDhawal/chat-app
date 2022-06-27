78fa68e938c11f5a0170e7fe9cca216ba10a1a51
tiwariDhawal <98008402+tiwariDhawal@users.noreply.github.com>
2022-06-27 08:22:56 +0530

    created profile-context component for using it in app and public and private route components

---
+++ src/context/profile.context.js
@@ -0,0 +1,13 @@
+import { createContext, useContext, useState } from "react";
+
+const ProfileContext = createContext();
+export const ProfileProvider = ({ children }) => {
+  const [profile] = useState(false);
+  return (
+    <ProfileContext.Provider value={profile}>
+      {children}
+    </ProfileContext.Provider>
+  );
+};
+
+export const useProfile = () => useContext(ProfileContext);
