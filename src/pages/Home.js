458d46d61cab78a66a255845e4af73d2bc31f55b
tiwariDhawal <98008402+tiwariDhawal@users.noreply.github.com>
2022-06-29 09:33:48 +0530

    started working with the chat lists and also used the timeago library for display the time

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Grid, Row, Col } from 'rsuite';
-import Sidebar from '../components/Sidebar';
+import React from "react";
+import { Grid, Row, Col } from "rsuite";
+import Sidebar from "../components/Sidebar";
 
 const Home = () => {
   return (
     <Grid fluid className="h-100">
-      <Row>
-        <Col xs={24} md={8}>
+      <Row className="h-100">
+        <Col xs={24} md={8} className="h-100">
           <Sidebar />
         </Col>
       </Row>
