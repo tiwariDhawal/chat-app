972448b14acebb4653aa751449e44146c85d345c
tiwariDhawal <98008402+tiwariDhawal@users.noreply.github.com>
2022-07-01 12:31:10 +0530

    handle delete mssg operation

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,43 +1,38 @@
-import React, { memo } from "react";
-import { Button } from "rsuite";
-import TimeAgo from "timeago-react";
-import { useCurrentRoom } from "../../../context/current-room.context";
-import { useHover, useMediaQuery } from "../../../misc/custom-hooks";
-import { auth } from "../../../misc/firebase";
-import PresenceDot from "../../PresenceDot";
-import ProfileAvatar from "../../ProfileAvatar";
-import IconBtnControl from "./IconBtnControl";
-import ProfileInfoBtnModal from "./ProfileInfoBtnModal";
+import React, { memo } from 'react';
+import TimeAgo from 'timeago-react';
+import { Button } from 'rsuite';
+import ProfileAvatar from '../../ProfileAvatar';
+import ProfileInfoBtnModal from './ProfileInfoBtnModal';
+import PresenceDot from '../../PresenceDot';
+import { useCurrentRoom } from '../../../context/current-room.context';
+import { auth } from '../../../misc/firebase';
+import { useHover, useMediaQuery } from '../../../misc/custom-hooks';
+import IconBtnControl from './IconBtnControl';
 
-const MessageItem = ({ message, handleAdmin, handleLike }) => {
+const MessageItem = ({ message, handleAdmin, handleLike, handleDelete }) => {
   const { author, createdAt, text, likes, likeCount } = message;
 
   const [selfRef, isHovered] = useHover();
+  const isMobile = useMediaQuery('(max-width: 992px)');
 
-  const isMobile = useMediaQuery("(max-width:992px)");
-
-  const isAdmin = useCurrentRoom((v) => v.isAdmin);
-
-  const admins = useCurrentRoom((v) => v.admins);
+  const isAdmin = useCurrentRoom(v => v.isAdmin);
+  const admins = useCurrentRoom(v => v.admins);
 
   const isMsgAuthorAdmin = admins.includes(author.uid);
   const isAuthor = auth.currentUser.uid === author.uid;
-
   const canGrantAdmin = isAdmin && !isAuthor;
 
   const canShowIcons = isMobile || isHovered;
-
   const isLiked = likes && Object.keys(likes).includes(auth.currentUser.uid);
 
   return (
     <li
-      className={`padded  mb-1 cursor-pointer ${
-        isHovered ? "bg-black-02" : ""
-      }`}
+      className={`padded mb-1 cursor-pointer ${isHovered ? 'bg-black-02' : ''}`}
       ref={selfRef}
     >
       <div className="d-flex align-items-center font-bolder mb-1">
         <PresenceDot uid={author.uid} />
+
         <ProfileAvatar
           src={author.avatar}
           name={author.name}
@@ -53,8 +48,8 @@ const MessageItem = ({ message, handleAdmin, handleLike }) => {
           {canGrantAdmin && (
             <Button block onClick={() => handleAdmin(author.uid)} color="blue">
               {isMsgAuthorAdmin
-                ? "Remove admin permission"
-                : "Give admin in this room"}
+                ? 'Remove admin permission'
+                : 'Give admin in this room'}
             </Button>
           )}
         </ProfileInfoBtnModal>
@@ -64,13 +59,21 @@ const MessageItem = ({ message, handleAdmin, handleLike }) => {
         />
 
         <IconBtnControl
-          {...(isLiked ? { color: "red" } : {})}
+          {...(isLiked ? { color: 'red' } : {})}
           isVisible={canShowIcons}
           iconName="heart"
-          tooltip="Like the message"
+          tooltip="Like this message"
           onClick={() => handleLike(message.id)}
           badgeContent={likeCount}
         />
+        {isAuthor && (
+          <IconBtnControl
+            isVisible={canShowIcons}
+            iconName="close"
+            tooltip="Delete this message"
+            onClick={() => handleDelete(message.id)}
+          />
+        )}
       </div>
 
       <div>
