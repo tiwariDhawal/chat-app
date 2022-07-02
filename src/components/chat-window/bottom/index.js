714a11f5d5f93787521a3f929cc7d5bddf5e33ae
tiwariDhawal <98008402+tiwariDhawal@users.noreply.github.com>
2022-07-02 09:24:20 +0530

    created attachment button with uploader component

--- src/components/chat-window/bottom/index.js
+++ src/components/chat-window/bottom/index.js
@@ -1,9 +1,10 @@
-import React, { useState, useCallback } from 'react';
-import { InputGroup, Input, Icon, Alert } from 'rsuite';
-import firebase from 'firebase/app';
-import { useParams } from 'react-router';
-import { useProfile } from '../../../context/profile.context';
-import { database } from '../../../misc/firebase';
+import React, { useState, useCallback } from "react";
+import { InputGroup, Input, Icon, Alert } from "rsuite";
+import firebase from "firebase/app";
+import { useParams } from "react-router";
+import { useProfile } from "../../../context/profile.context";
+import { database } from "../../../misc/firebase";
+import AttachmentBtnModal from "./AttachmentBtnModal";
 
 function assembleMessage(profile, chatId) {
   return {
@@ -15,23 +16,23 @@ function assembleMessage(profile, chatId) {
       ...(profile.avatar ? { avatar: profile.avatar } : {}),
     },
     createdAt: firebase.database.ServerValue.TIMESTAMP,
-    likeCount :0,
+    likeCount: 0,
   };
 }
 
 const Bottom = () => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   const { chatId } = useParams();
   const { profile } = useProfile();
 
-  const onInputChange = useCallback(value => {
+  const onInputChange = useCallback((value) => {
     setInput(value);
   }, []);
 
   const onSendClick = async () => {
-    if (input.trim() === '') {
+    if (input.trim() === "") {
       return;
     }
 
@@ -40,7 +41,7 @@ const Bottom = () => {
 
     const updates = {};
 
-    const messageId = database.ref('messages').push().key;
+    const messageId = database.ref("messages").push().key;
 
     updates[`/messages/${messageId}`] = msgData;
     updates[`/rooms/${chatId}/lastMessage`] = {
@@ -52,7 +53,7 @@ const Bottom = () => {
     try {
       await database.ref().update(updates);
 
-      setInput('');
+      setInput("");
       setIsLoading(false);
     } catch (err) {
       setIsLoading(false);
@@ -60,7 +61,7 @@ const Bottom = () => {
     }
   };
 
-  const onKeyDown = ev => {
+  const onKeyDown = (ev) => {
     if (ev.keyCode === 13) {
       ev.preventDefault();
       onSendClick();
@@ -70,6 +71,7 @@ const Bottom = () => {
   return (
     <div>
       <InputGroup>
+        <AttachmentBtnModal />
         <Input
           placeholder="Write a new message here..."
           value={input}
